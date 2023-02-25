e7e52c4680574ecc870a29d92c9ebb0aace95622
Guuti <gustavo.silva286@fatec.sp.gov.br>
2023-02-24 21:50:59 -0300

    aula3 exercicio

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-alert("Olá")
+//alert("Olá")
+
+const a = Number(prompt("Digite seu salário:"))
+const b = Number(prompt("Digite o percentual de aumento:"))
+
+//const lista = [a,b]
+
+//let resultado = a+b
+//console.log(resultado)
+
+resultado = a*((b/100)+1)
+console.log(resultado) 
+
+document.write (resultado)
